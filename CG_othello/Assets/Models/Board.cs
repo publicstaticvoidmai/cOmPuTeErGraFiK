d6eff9f89087a604baabaad6c93cb14fac81110d
publicstaticvoidmai <mqngyn@gmail.com>
2019-06-15 16:13:59 +0200

    some tests idk

--- CG_othello/Assets/Models/Board.cs
+++ CG_othello/Assets/Models/Board.cs
@@ -6,6 +6,38 @@ namespace Models
     
     public class Board : MonoBehaviour
     {
+
+        public Piece[,] pieces = new Piece[8, 8];
+        public GameObject whitePref;
+        public GameObject blackPref;
+
+        private void Start()
+        {
+            GenerateBoard();
+        }
+
+        private void GenerateBoard()
+        {
+            GeneratePiece(0,0);
+            GeneratePiece(1,0);
+        }
+
+        private void GeneratePiece(int x, int z)
+        {
+            GameObject go = Instantiate(whitePref) as GameObject;
+            go.transform.SetParent(transform);
+            Piece p = go.GetComponent<Piece>();
+            pieces[x, z] = p;
+            MovePiece(p, x, z);
+
+        }
+
+        private void MovePiece(Piece p, int x, int z)
+        {
+            p.transform.position = new Vector3(x, 0, z);
+
+        }
+
         private void OnMouseOver()
         {
             //If your mouse hovers over the GameObject with the script attached, output this message
